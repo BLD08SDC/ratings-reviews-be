0cb55a8d1aa058b777c744703f5bc14cc40e3e8c
thomHayner <thom.hayner@gmail.com>
2020-04-02 15:11:52 -0600

    Update: REFERENCES, Order of Table Creation

--- db.sql
+++ db.sql
@@ -8,34 +8,6 @@ CREATE DATABASE ratings-reviews-be
 
 USE DATABASE ratings-reviews-be
 
-CREATE TABLE "characteristics" (
-  "id" SERIAL,
-  "product_id" INTEGER NOT NULL,
-  "name" VARCHAR,
-  PRIMARY KEY ("id")
-);
-
-CREATE INDEX "FK" ON  "characteristics" ("product_id");
-
-CREATE TABLE "reviews_photos" (
-  "id" SERIAL,
-  "review_id" INTEGER NOT NULL,
-  "url" VARCHAR,
-  PRIMARY KEY ("id")
-);
-
-CREATE INDEX "FK" ON  "reviews_photos" ("review_id");
-
-CREATE TABLE "characteristics_reviews" (
-  "id" SERIAL,
-  "character_id" INTEGER NOT NULL,
-  "review_id" INTEGER NOT NULL,
-  "value",
-  PRIMARY KEY ("id")
-);
-
-CREATE INDEX "FK" ON  "characteristics_reviews" ("character_id", "review_id");
-
 CREATE TABLE "products" (
   "id" SERIAL,
   "name" VARCHAR,
@@ -46,15 +18,36 @@ CREATE TABLE "products" (
   PRIMARY KEY ("id")
 );
 
+CREATE TABLE "characteristics" (
+  "id" SERIAL,
+  "product_id" INTEGER NOT NULL REFERENCES "products"("id"),
+  "name" VARCHAR,
+  PRIMARY KEY ("id")
+);
+
+--CREATE INDEX "FK" ON  "characteristics" ("product_id");
+
+
+CREATE TABLE "characteristics_reviews" (
+  "id" SERIAL,
+  "character_id" INTEGER NOT NULL REFERENCES "characteristics"("id"),
+  "review_id" INTEGER NOT NULL,
+  "value" VARCHAR,
+  PRIMARY KEY ("id")
+);
+
+-- CREATE INDEX "FK" ON  "characteristics_reviews" ("character_id", "review_id");
+
+
 CREATE TABLE "reviews" (
   "id" SERIAL,
-  "product_id" INTEGER NOT NULL,
+  "product_id" INTEGER NOT NULL REFERENCES "products"("id"),
   "rating" INTEGER,
   "date" DATE,
   "summary" VARCHAR,
   "body" VARCHAR,
   "recommend" BOOLEAN,
-  "reported",
+  "reported" INTEGER,
   "reviewer_name" VARCHAR,
   "reviewer_email" VARCHAR,
   "response" VARCHAR,
@@ -62,18 +55,27 @@ CREATE TABLE "reviews" (
   PRIMARY KEY ("id")
 );
 
-CREATE INDEX "FK" ON  "reviews" ("product_id");
+--CREATE INDEX "FK" ON  "reviews" ("product_id");
+
+CREATE TABLE "reviews_photos" (
+  "id" SERIAL,
+  "review_id" INTEGER NOT NULL REFERENCES "reviews"("id"),
+  "url" VARCHAR,
+  PRIMARY KEY ("id")
+);
+
+--CREATE INDEX "FK" ON  "reviews_photos" ("review_id");
 
 --==--==-==-==-==-==-==-==-==-==-==-==--==-==-==-==-==-==-==-==-==-==-==--==-==-==-==-==-==-==-==-==-==-==--==-==-==-==-==-==-==-==-==-==--
 --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
 --==--==-==-==-==-==-==-==-==-==-==-==--==-==-==-==-==-==-==-==-==-==-==--==-==-==-==-==-==-==-==-==-==-==--==-==-==-==-==-==-==-==-==-==--
 
-COPY "characteristics" ("product_id", "name") FROM characteristics.csv
+COPY "products" ("name", "slogan", "description", "category", "default_price") FROM /Users/thomas/git-hackreactor/systemDesignCapstone/ratings-reviews-be/rawDataCSV/product.csv
 
-COPY "reviews_photos" ("review_id", "url") FROM reviews_photos.csv
+COPY "characteristics" ("product_id", "name") FROM /Users/thomas/git-hackreactor/systemDesignCapstone/ratings-reviews-be/rawDataCSV/characteristics.csv
 
-COPY "characteristics_reviews" ("character_id", "review_id", "value") FROM characteristic_reviews.csv
+COPY "characteristics_reviews" ("character_id", "review_id", "value") FROM /Users/thomas/git-hackreactor/systemDesignCapstone/ratings-reviews-be/rawDataCSV/characteristic_reviews.csv
 
-COPY "products" ("name", "slogan", "description", "category", "default_price") FROM product.csv
+COPY "reviews" ("product_id", "rating", "date", "summary", "body", "recommend", "reported", "reviewer_name", "reviewer_email", "response", "helpfulness") FROM /Users/thomas/git-hackreactor/systemDesignCapstone/ratings-reviews-be/rawDataCSV/reviews.csv
 
-COPY "reviews" ("product_id", "rating", "date", "summary", "body", "recommend", "reported", "reviewer_name", "reviewer_email", "response", "helpfulness") FROM reviews.csv
+COPY "reviews_photos" ("review_id", "url") FROM /Users/thomas/git-hackreactor/systemDesignCapstone/ratings-reviews-be/rawDataCSV/reviews_photos.csv
