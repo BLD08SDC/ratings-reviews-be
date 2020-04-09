23f9643b7c8cd8a77082b8ec42c9259d7983ee9a
thomHayner <thom.hayner@gmail.com>
2020-04-09 01:15:51 -0600

    Add req.params, Cleanup - Fix Typo

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.get('/reviews/:product_id/list', (req, res) => {
             .catch(error => console.log(error))
 })
 
-app/put('/reviews/helpful/:review_id', (req, res) => {
-    queryPool.markHelpful()
+app.put('/reviews/helpful/:review_id', (req, res) => {
+    queryPool.markHelpful(req.params)
         .then(res.send('Thank\'s for the feedback'))
         .catch(error => console.log(error))
 })
