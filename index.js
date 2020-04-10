6ebdb5d03f3f0f1a75ecacebacbbf32eacc0928e
thomHayner <thom.hayner@gmail.com>
2020-04-10 16:43:31 -0600

    Comments / Cleanup

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+// import Express
 const express = require('express');
+
+// import your pool
 const queryPool = require('./queries');
+
 const app = express();
 const port = 3000;
 
+// These app.use statements tell Express which middleware 'layers' to apply
 app.use(express.json());
 app.use(
     express.urlencoded({
@@ -10,6 +15,7 @@ app.use(
     })
 );
 
+// This is the API route for getting a list of reviews (currently returns duplicates if review has more than one photo attached to it)
 app.get('/reviews/:product_id/list', (req, res) => {
     queryPool.getListOfReviews(req.params)
             .then(data => {
@@ -37,6 +43,7 @@ app.get('/reviews/:product_id/list', (req, res) => {
             .catch(error => console.log(error))
 })
 
+// This is the API route for getting the characteristics meta data (incomplete / currently returns nothing)
 app.get('reviews/:product_id/meta', (req, res) => {
     queryPool.getCharacteristicsMeta(req.params)
         .then(res.send(
@@ -47,24 +54,28 @@ app.get('reviews/:product_id/meta', (req, res) => {
         .catch(error => console.log(error))
 })
 
+// This is the API route for adding a new review (does not currently handle adding a photo)
 app.post('/reviews/:product_id', (req, res) => {
     queryPool.addReview(req.params)
         .then(res.send("Thank\'s for your review!"))
         .catch(error => console.log(error))
 })
 
+// This is the API route for marking a review as helpful
 app.put('/reviews/helpful/:review_id', (req, res) => {
     queryPool.markHelpful(req.params)
         .then(res.send('Thank\'s for the feedback'))
         .catch(error => console.log(error))
 })
 
+// This is the API route for reporting and review
 app.put('/reviews/report/:review_id', (req, res) => {
     queryPool.reportReview(req.params)
         .then(res.send('Review Reported'))
         .catch(error => console.log(error))
 })
 
+// Express need to know what port to listen on, a console.log callback serves as a confirmation that the server is properly functioning and running
 app.listen(port, () => {
     console.log(`the server is running on port ${port}`)
 });
